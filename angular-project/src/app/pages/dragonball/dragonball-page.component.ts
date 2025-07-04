c8774c1d9e7c4d65069160620a39914d32ecbdba
carlos.garita <carlos.garita@factork-software.com>
2025-07-04 09:44:32 -0600

    Feat: @if practice

--- angular-project/src/app/pages/dragonball/dragonball-page.component.ts
+++ angular-project/src/app/pages/dragonball/dragonball-page.component.ts
@@ -15,6 +15,8 @@ export class DragonballComponent {
     characters = signal<Character[]>([
         { id: 1, name: 'Goku', power: 9000 },
         { id: 2, name: 'Vegeta', power: 8000 },
+        { id: 4, name: 'Yamcha', power: 500 },
         { id: 3, name: 'Piccolo', power: 3000 },
+        
     ]);
 }
