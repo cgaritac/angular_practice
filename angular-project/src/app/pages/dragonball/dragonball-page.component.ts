493918a4355e39985df6741891b7002ea0d43b1b
carlos.garita <carlos.garita@factork-software.com>
2025-10-28 23:41:45 -0600

    Feat: Inputs

--- angular-project/src/app/pages/dragonball/dragonball-page.component.ts
+++ angular-project/src/app/pages/dragonball/dragonball-page.component.ts
@@ -12,6 +12,9 @@ interface Character {
 })
 
 export class DragonballComponent {
+    name = signal('Gohan');
+    power = signal(100);
+
     characters = signal<Character[]>([
         { id: 1, name: 'Goku', power: 9000 },
         { id: 2, name: 'Vegeta', power: 8000 },
@@ -19,4 +22,21 @@ export class DragonballComponent {
         { id: 3, name: 'Piccolo', power: 3000 },
         
     ]);
+
+    addCharacter() {
+        if (!this.name() || !this.name().trim() || this.power() <= 0) return;
+        const newCharacter: Character = {
+            id: this.characters().length + 1,
+            name: this.name(),
+            power: this.power(),
+        }
+        //this.characters().push(newCharacter); Con senales no se recomienda usar el push para agregar un nuevo elemento, se recomienda usar el update para actualizar el estado de la señal.
+        this.characters.update((list) => [...list, newCharacter]);
+        this.resetFields();
+    }
+
+    resetFields() {
+        this.name.set('');
+        this.power.set(0);
+    }
 }
